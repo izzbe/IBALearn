30a8d3335e026eb6faf080a3fb835382a52f9e1f
Ian Zhang <ianzhang08030612@gmail.com>
2025-03-09 15:51:50 -0400

    Set up IBATensor.h

--- IBATensor/IBATensor.h
+++ IBATensor/IBATensor.h
@@ -2,9 +2,28 @@
 // Created by ianzh on 3/9/2025.
 //
 
+#include <vector>
+
 #ifndef IBATENSOR_H
 #define IBATENSOR_H
 class Tensor {
+    std::vector<float>;
+    std::vector<size>;
+    std::vector<stride>;
+
+public:
+    Tensor(std::vector<float> values, std::vector<int> shape);
+    int index(std::vector<int> indices) const;
+    float get(std::vector<int> indices) const;
+    void set(std::vector<int> indices, float value);
+    void print_shape() const;
+
+    Tensor operator*(Tensor other);
+    Tensor operator+(Tensor other); //element wise addition
+    Tensor operator/(Tensor other); //element wise division
+    Tensor operator-(Tensor other); //element wise subtraction
+
+    Tensor apply(Tensor other, float (*func)(float));
 
 };
 
